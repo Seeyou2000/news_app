00bd948b8fee9b25509d8b5db43d7855f8276a7d
Kimsiwoo <valentine0517@naver.com>
2023-11-25 16:37:19 +0900

    categoryboundary 분리

---
+++ lib/boundary/categoryboundary.dart
@@ -0,0 +1,45 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:news_app/pages/categorypage.dart';
+
+class CategoryBoundary extends StatelessWidget {
+  final String categoryName, imageUrl;
+  CategoryBoundary({this.categoryName = '', this.imageUrl = ''});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) => CategoryPages(
+          category: categoryName.toLowerCase(),
+        ),
+        ));
+      },
+      child: Container(
+        margin: const EdgeInsets.only(right: 10.0),
+        child: Stack(
+          children: <Widget>[
+            ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: CachedNetworkImage(imageUrl: imageUrl, width: 150, height: 80, fit: BoxFit.cover,)),
+            Container(
+              alignment: Alignment.center,
+              width: 150, height: 90,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.black12,
+              ),
+              child: Text(categoryName,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
